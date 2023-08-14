531535f155a9cf238b631662f30f047849f84a47
dongliangma <dongliangma@cybstar.com>
2023-08-14 21:54:05 +0800

    使用上nacos

--- security-wasteGeneration/src/main/java/com/security/wasteGeneration/service/WasteGenerationService.java
+++ security-wasteGeneration/src/main/java/com/security/wasteGeneration/service/WasteGenerationService.java
@@ -62,6 +62,9 @@ public class WasteGenerationService {
      * 校验所选的产生单子是否都存在，并且都是“待处置”状态（status为0）、去向都是“贮存”（destination为1）
      */
     public boolean checkGenerationStatusIs0AndDestinationIs1(List<String> generationIds) {
+    
+        System.out.println("远程调用进来了----------------------");
+        
         Assert.notEmpty(generationIds, "产生单ids不能为空");
         List<HseHazardousWasteGeneration> generations = wasteGenerationMapper.selectBatchIds(generationIds);
         if (generations.size() != generationIds.size()) {
