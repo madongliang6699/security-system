a94475feac1fb27d02e846dd7fa1494e4f8d2575
mdl <mdl@163.com>
2023-11-01 00:58:30 +0800

    开始并发编程1

---
+++ security-study/src/main/java/com/security/study/特定知识点测试和总结/webSocket/TestWebSocketController.java
@@ -0,0 +1,27 @@
+package com.security.study.特定知识点测试和总结.webSocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/testWebSocket")
+public class TestWebSocketController {
+
+
+    @Resource
+    WebSocketServer webSocketServer;
+
+    @RequestMapping("/send")
+    public void sendMsg() throws InterruptedException {
+        for(;;){
+            TimeUnit.SECONDS.sleep(3);
+            System.out.println("发送一次---");
+            webSocketServer.sendToAll(String.valueOf(new Random().nextInt(1000)));
+        }
+    }
+}
