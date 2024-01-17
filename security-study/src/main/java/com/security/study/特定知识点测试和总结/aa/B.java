591e88db44ba2e02495251d33e345238f10b8684
dongliangma <dongliangma@cybstar.com>
2024-01-17 09:52:54 +0800

    测试集合操作

---
+++ security-study/src/main/java/com/security/study/特定知识点测试和总结/aa/B.java
@@ -0,0 +1,44 @@
+package com.security.study.特定知识点测试和总结.aa;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class B {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.add(3);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(4);
+
+        //获取并集
+        Collection<Integer> unionList = CollectionUtils.union(list, list2);
+        System.out.println(unionList);
+
+        //获取交集
+        Collection<Integer> intersectionList = CollectionUtils.intersection(list, list2);
+        System.out.println(intersectionList);
+
+        //获取交集的补集
+        Collection<Integer> disjunctionList = CollectionUtils.disjunction(list, list2);
+        System.out.println(disjunctionList);
+
+        //获取差集
+        Collection<Integer> subtractList = CollectionUtils.subtract(list, list2);
+        System.out.println(subtractList);
+
+        //获取差集
+        Collection<Integer> subtractList2 = CollectionUtils.subtract(list2, list);
+        System.out.println(subtractList2);
+
+    }
+
+}
