b5c891eae14014bb2d9d2fbc3195ecb2bde231a6
dongliangma <dongliangma@cybstar.com>
2024-06-05 18:25:59 +0800

    总结pom中的<resources>标签

--- security-inventory/src/main/java/com/security/inventory/service/impl/InventoryServiceImpl.java
+++ security-inventory/src/main/java/com/security/inventory/service/impl/InventoryServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.security.inventory.domain.request.LockProductStockRequest;
 import com.security.inventory.mapper.InventoryMapper;
 import com.security.inventory.model.entity.ProductStockDO;
+import com.security.inventory.other.exception.InventoryBizException;
+import com.security.inventory.other.exception.InventoryErrorCodeEnum;
 import com.security.inventory.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,19 +30,25 @@ public class InventoryServiceImpl extends ServiceImpl<InventoryMapper, ProductSt
      */
     @Transactional(rollbackFor = Exception.class) //todo 这里注释掉，好像全局事务也能回滚本地的事务，使本地方法内的多个修改也能做到数据一致性
     public Boolean lockProductStock(LockProductStockRequest lockProductStockRequest) {
-        /*
-         这里固定使用id为1和2的数据，模拟当前订单中买了两个商品，分别是两种sku的具体商品，锁定这两个sku的库存。
-         */
 
-        ProductStockDO byId = getById(lockProductStockRequest.getProductId());
-        byId.setSaleStockQuantity(byId.getSaleStockQuantity() - lockProductStockRequest.getQuantity());
-        byId.setLockedStockQuantity(byId.getLockedStockQuantity() + lockProductStockRequest.getQuantity());
-        updateById(byId);
+        //todo 下面这里的先获取数据,然后修改记录并保存的操作是有并发安全问题的, 因为当多个线程同时执行了getById方法,获取的数据是一样的, 然后又同时在一样数据的基础上执行下面的updateById数据到数据库, 导致数据错误.
+        //ProductStockDO productStockDO = this.getById(lockProductStockRequest.getProductId());
 
-        /*
-          todo 这里先测试本地事务是否可用，然后测试seata全局事务是否可用。
+
+        /**
+         * todo 改进的方式有很多:
+         *  第一种:查询的时候使用select for update的sql. (要小心避免死锁)
+         *  第二种:直接一个sql完成修改操作, 不过这种方式不好判断库存是不是不足, 也需要sql判断.
+         *  第三种:使用锁, 把查询和修改的过程放在一个原子锁里.
          */
-//        int i = 9 / 0;
+        ProductStockDO productStockDO = inventoryMapper.selectStockForUpdate(lockProductStockRequest.getProductId());
+        if(productStockDO.getSaleStockQuantity() <= lockProductStockRequest.getQuantity()){
+            throw new InventoryBizException(InventoryErrorCodeEnum.PRODUCT_SKU_STOCK_Insufficient);
+        }
+
+        productStockDO.setSaleStockQuantity(productStockDO.getSaleStockQuantity() - lockProductStockRequest.getQuantity());
+        productStockDO.setLockedStockQuantity(productStockDO.getLockedStockQuantity() + lockProductStockRequest.getQuantity());
+        this.updateById(productStockDO);
 
         return true;
     }
